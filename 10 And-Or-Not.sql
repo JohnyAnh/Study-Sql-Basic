4e4bb2dd2c3a53997ac8702836a4d53c85e9964b
Johny Vu <johnyanhn01@gmail.com>
2023-07-31 14:47:12 +0700

    And-Or-Not

--- 10 And-Or-Not.sql
+++ 10 And-Or-Not.sql
@@ -21,5 +21,13 @@ SELECT *
 FROM Products
 WHERE NOT UnitPrice >= 100 AND CategoryID= 1;
 
+--Bt Liet ke tat ca don hang co gia van chuyen (Freight) trong khoang [50,100] do la
+SELECT *
+FROM Orders
+WHERE Freight >= 50 AND Freight <= 100;
 
-
+--Bt Hay liet cac san pham co so hang trong kho (UnitslnStock) lon hon 20 va so
+-- luong hang trong don hang (UnitsOrder) nho hon 20
+SELECT *
+FROM Products
+WHERE UnitsInStock > 20 AND UnitsOnOrder < 20;
